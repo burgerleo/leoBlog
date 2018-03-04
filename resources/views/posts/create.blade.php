d10ae5062bc730f1ad974ea0c4792db4a2051e52
burger43 <e455285@gmail.com>
2018-03-04 22:54:34 +0800

    加入文字編輯器成功 正在調適

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+    <!-- <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script> -->
+    <!-- <script src="/vendor/unisharp/laravel-ckeditor/adapters/jquery.js"></script> -->
+    <script src="{{asset('/vendor/unisharp/laravel-ckeditor/ckeditor.js')}}"></script>
+    <script src="{{asset('/vendor/unisharp/laravel-ckeditor/adapters/jquery.js')}}"></script>
+
+    
 <div><label>標題</label>
 	<input type="text" name="tittle">
 </div>
+
+<textarea name="body">
+
+</textarea>
+
+<button id="alertContent">取得HTML</button>
+<script>
+    CKEDITOR.replace('body');
+    $('#alertContent').click(function(){
+	 	// CKEDITOR.instances.editorDemo.updateElement();
+		// console.log($('textarea[name=body]').val());
+		console.log(CKEDITOR.instances.body.getData());
+	});
+</script>
+
 @endsection
